a7dc209d6efe2cff1e11debb946be626f3342d81
Dinu Moksh <mdinakar@gmail.com>
2022-03-07 22:48:32 -0500

    added alrair chart, yards_gained and qb_epa with better filtering for pass attempts

--- pg.py
+++ pg.py
@@ -11,20 +11,10 @@ df = nflfastpy.load_pbp_data("2021")
 engine = create_engine('postgresql://dinu:@localhost:5432/dinu')
 
 #load df into the table.
-#df.to_sql("last_year",engine,schema="nfl",if_exists="replace")
+df.to_sql("last_year",engine,schema="nfl",if_exists="replace")
 
-pysqldf = lambda q: sqldf(q, globals())
 
-qb_eff_df = pysqldf("""select 
-passer_player_id,passer_player_name,
-SUM(1) as attempts,
-SUM(CASE WHEN (first_down_pass=1 or pass_touchdown=1) THEN 1 ELSE 0 END) as successful_attempts,
-CAST(ROUND((CAST(SUM(CASE WHEN (first_down_pass=1 or pass_touchdown=1) THEN 1 ELSE 0 END) as decimal)/CAST(SUM(1) as decimal))*100,2) as decimal) as efficiency
-from df
-where  down in (3,4) and play_type_nfl like 'PASS' and pass = 1
-group by passer_player_id,passer_player_name
-order by successful_attempts desc;""")
 
-print(qb_eff_df.info(),qb_eff_df)
-#st.dataframe(qb_eff_df)
+
+
 
--- qb_eff.py
+++ qb_eff.py
@@ -1,6 +1,7 @@
 import nflfastpy
 import pandas as pd
 import streamlit as st  
+import altair as alt
 from pandasql import sqldf
 
 #download play by play data
@@ -17,12 +18,19 @@ df = load_data()
 
 print(df.shape)
 #filter for 3rd and 4th downs with pass attempts
-df = df.loc[((df['down'] == 3) | (df['down'] == 4)) & (df['play_type_nfl'] == 'PASS') & (df['pass'] == 1)]
+df = df.loc[((df['down'] == 3) | (df['down'] == 4)) & ((df['play_type'] == 'pass') | (df['pass'] == 1))]
 
 #now group by QB and count attemps (straight up count) and successful attempts (SUM(CASE WHEN (first_down_pass=1 or pass_touchdown=1) THEN 1 ELSE 0 END))
-df = df.groupby(['passer_player_id','passer_player_name'], as_index=False).apply(lambda x: pd.Series({'attempts':x['down'].count(),
-'successful_attempts':x.loc[(x.first_down_pass==1)|(x.pass_touchdown==1)]['down'].count()}))
+df = df.groupby(['passer_id','passer'], as_index=False).apply(lambda x: pd.Series({'attempts':x['down'].count(),
+'successful_attempts':x.loc[(x.first_down_pass==1)|(x.pass_touchdown==1)]['down'].count(),
+'total_yards':x['yards_gained'].sum(),
+'qb_epa':x['qb_epa'].sum()}))
 
-df['sucess_percent'] = round((df['successful_attempts']/df['attempts'])*100,2)
+df['success_percent'] = round((df['successful_attempts']/df['attempts'])*100,2)
 
 st.dataframe(df)
+
+c = alt.Chart(df).mark_circle().encode(
+     x='successful_attempts', y='attempts', size='success_percent', color='successful_attempts', tooltip=['attempts', 'successful_attempts','passer','success_percent'])
+
+st.altair_chart(c, use_container_width=True)
