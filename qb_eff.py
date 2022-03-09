b7699ed99a546e7fe584334dfd17e9898b6ad793
Dinu Moksh <mdinakar@gmail.com>
2022-03-08 20:52:27 -0500

    added plotly chart for epa and more metrics on the top

--- qb_eff.py
+++ qb_eff.py
@@ -1,8 +1,11 @@
 import nflfastpy
+from numpy import sort
 import pandas as pd
 import streamlit as st  
 import altair as alt
 from pandasql import sqldf
+import plotly_express as px
+
 
 #download play by play data
 
@@ -23,23 +26,43 @@ df = df.loc[((df['down'] == 3) | (df['down'] == 4)) & ((df['play_type'] == 'pass
 df = df.groupby(['passer_id','passer'], as_index=False).apply(lambda x: pd.Series({'attempts':x['down'].count(),
 'successful_attempts':x.loc[(x.first_down_pass==1)|(x.pass_touchdown==1)]['down'].count(),
 'total_yards':x['yards_gained'].sum(),
-'qb_epa':x['qb_epa'].sum()}))
+'qb_epa':x['qb_epa'].sum(),
+'touchdown':x['touchdown'].sum()}))
 
 df['success_percent'] = round((df['successful_attempts']/df['attempts'])*100,2)
 
-#title and captions
-st.title("🏈 NFL QB Performance on 3rd and 4th downs")
-st.caption("* An attempt is considered successful if it ends in firstdown or touchdown, with or without the help of penalties.")
-st.caption("* Both regular and post season games")
+#sort df by successful_attemps descending - this is default
+df.sort_values(by=['successful_attempts'],ascending=False,inplace=True)
 
-#snippets
+#change df column types
+
+df.attempts = df.attempts.astype(int)
+df.successful_attempts = df.successful_attempts.astype(int)
+
+#title and captions
+st.title("🏈 QB Stats on 3rd and 4th downs")
+st.caption("An attempt is considered successful if it ends in firstdown or touchdown, with or without the help of penalties.")
+#st.caption("* Both regular and post season games")
+
+#snippets with columns
 #st.write("Burrow has most attemps at 242 and Stafford most successful attempts at 99")
 #st.write("For players above 100 attempts, Justin Fields has lowest sucessful attempts with 23")
-st.metric(label="Most attempts - Burrow", value="242")
-st.metric(label="Most successful attempts - Stafford", value="99")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.metric(label="Most attempts - Burrow", value="242")
+with col2:
+    st.metric(label="Most successful attempts - Stafford", value="99")
+
+col3, col4 = st.columns(2)
+with col3:
+    st.metric(label="Most yards - Stafford", value="1706")
+with col4:
+    st.metric(label="Most touchdowns - Stafford", value="22")
 
 #display the dataframe as table - remove passer_id
-st.dataframe(df[['passer','attempts','successful_attempts','success_percent','qb_epa','total_yards']])
+st.subheader("Table - QB Stats on 3rd and 4th downs")
+st.dataframe(df[['passer','attempts','successful_attempts','success_percent','qb_epa','total_yards','touchdown']])
 
 #plot to show attempts vs successful_attempts
 st.subheader("Plot - Attempts vs Successful Attempts")
@@ -48,3 +71,11 @@ c = alt.Chart(df).mark_circle().encode(
 
 
 st.altair_chart(c, use_container_width=True)
+
+
+
+#chart to show qb_epa
+df.sort_values(by=['qb_epa'],ascending=True,inplace=True)
+st.subheader("Chart - QB EPA")
+fig = px.bar(df, x='passer', y='qb_epa')
+st.plotly_chart(figure_or_data=fig)
